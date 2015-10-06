3a8e5a1cdd56f76512270349747acc6ad4478327
oaestay <oaestay@uc.cl>
2015-10-06 18:19:38 -0300

    Added a username, a password and an email to users

--- Entrega 2/savemesensei/db/migrate/20151006142210_create_users.rb
+++ Entrega 2/savemesensei/db/migrate/20151006142210_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-        t.string :first_name
-        t.string :last_name
-        t.string :location
         t.string :username
         t.string :password
         t.string :email
+        t.string :first_name
+        t.string :last_name
+        t.string :location
         t.integer :age
         t.string :gender
         t.string :image, default: 'pug.jpg'
